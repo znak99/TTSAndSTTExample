17d2b879dde3d7176d593009b867ce0b5202bc80
znak99 <xhahekcldhkznak0113@gmail.com>
2023-09-30 23:29:11 +0900

    Initial Commit

---
+++ TTSAndSTTExample/TTSAndSTTExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTSAndSTTExampleApp.swift
+//  TTSAndSTTExample
+//
+//  Created by SeungWoo Hong on 2023/09/30.
+//
+
+import SwiftUI
+
+@main
+struct TTSAndSTTExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
